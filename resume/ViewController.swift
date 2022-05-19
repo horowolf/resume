dca33b9746adf2b199ac183e4cd2dcdb152c8be6
horowolf <lucky801991@gmail.com>
2022-05-20 02:00:48 +0900

    Initial Commit

---
+++ resume/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  resume
+//
+//  Created by horo on 5/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
